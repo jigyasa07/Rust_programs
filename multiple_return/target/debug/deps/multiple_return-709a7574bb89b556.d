d509680b3a7992c5be5e27063e7b24ca22e8332b
jigyasa07 <51105890+jigyasa07@users.noreply.github.com>
2019-05-29 08:33:41 +0530

    Add files via upload

---
+++ multiple_return/target/debug/deps/multiple_return-709a7574bb89b556.d
@@ -0,0 +1,5 @@
+/home/user/rust/multiple_return/target/debug/deps/multiple_return-709a7574bb89b556: src/main.rs
+
+/home/user/rust/multiple_return/target/debug/deps/multiple_return-709a7574bb89b556.d: src/main.rs
+
+src/main.rs:
---
+++ multiple_return/target/debug/multiple_return.d
@@ -0,0 +1,1 @@
+/home/user/rust/multiple_return/target/debug/multiple_return: /home/user/rust/multiple_return/src/main.rs
